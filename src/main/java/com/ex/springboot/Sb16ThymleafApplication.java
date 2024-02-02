a78894bb1ba86ed61c2b94a305f8bac56b39c1ff
GREEN <GREEN@G704-11>
2024-02-02 10:10:31 +0900

    프로젝트 세팅

---
+++ src/main/java/com/ex/springboot/Sb16ThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.ex.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb16ThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb16ThymleafApplication.class, args);
+	}
+
+}
